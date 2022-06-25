386a88d0ce4c61d0c453edcf5f9e51c9f76b0eae
Wen Zhao (Joan) <withzhaowen@126.com>
2022-06-25 23:58:41 +0800

    update

--- src/main/java/com/github/ybqdren/MyApplication.java
+++ src/main/java/com/github/ybqdren/MyApplication.java
@@ -1,5 +1,8 @@
 package com.github.ybqdren;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author zhao wen
  * @sice 2022/6/25
@@ -7,5 +10,9 @@ package com.github.ybqdren;
 
 
 
+@SpringBootApplication
 public class MyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class,args);
+    }
 }
